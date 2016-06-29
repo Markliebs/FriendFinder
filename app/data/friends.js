d8834f1aff291cfd6757475c62e173e3680786cc
markliebs <markliebs@me.com>
2016-06-29 01:31:53 -0400

    Added home/survey pages and survey array of sample friends

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+//list of pre-set friends
+var friends = [
+	{
+		"name": "Ace",
+		"photo": "https://s-media-cache-ak0.pinimg.com/564x/8d/73/11/8d7311c6223d3a7be9617183dc169771.jpg",
+		"scores": [5,4,3,1,3,1,3,3,5,5]
+		//33
+	},
+	{
+		"name": "Bill   ",
+		"photo": "http://vignette2.wikia.nocookie.net/villains/images/f/ff/Buffalo_Bill.jpg/revision/latest?cb=20120918160002",
+		"scores": [1,5,4,1,4,5,1,4,5,4]
+		//34
+	},
+	{
+		"name": "Biff",
+		"photo": "http://vignette1.wikia.nocookie.net/bttf/images/7/7b/Griff-tannen-back-to-the-future-ii.jpg/revision/latest?cb=20111124103720",
+		"scores": [2,2,5,5,3,4,3,1,2,3]
+		//30
+	},
+	{
+		"name": "Ricky",
+		"photo": "http://www.monologuedb.com/wp-content/uploads/2011/03/will-ferrell-ricky-bobby-talladega-nights.jpg",
+		"scores": [4,3,3,5,5,3,5,3,1,4]
+		//36
+	},
+	{
+		"name": "Han, Mr. Solo if you're nasty",
+		"photo": "http://blogs-images.forbes.com/markhughes/files/2015/08/Han-Solo-Star-Wars-4.png",
+		"scores": [5,1,5,2,5,2,4,2,4,1]
+		//31
+	}
+];
+//sends to server
+module.exports = friends;
